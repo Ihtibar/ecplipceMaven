e934031dfda137a5237f64e8c222b5ac1f693c71
Ihtibar <46764819+Ihtibar@users.noreply.github.com>
2019-02-01 14:41:41 -0500

    new

---
+++ Testings/firstUploading/uploadingPackage/kkkkk.java
@@ -0,0 +1,11 @@
+public class Map2 {
+	public static void main(String[] args) {
+		 Map<Long, String> product= new HashMap<>();
+		 product.put(125L, "book");
+		 product.put(127L, "table");
+		 product.put(126L, "pen");
+		 product.put(122L, "bed");
+		 product.put(12322L, "pan");
+		 System.out.println(product);
+     }
+     }
