bab5d0b473a4dce95076f3fdc71df634a63deb36
ihtibar <entizar@gmail.com>
2019-02-01 14:10:44 -0500

    kkk

--- Testings/firstUploading/uploadingPackage/IhtibarCases.java
+++ Testings/firstUploading/uploadingPackage/IhtibarCases.java
@@ -4,6 +4,7 @@ public class IhtibarCases {
 	public static void main(String[] args) {
 		System.out.println("Hello gitHub");
 		System.out.println("second test");
+		System.out.println("men ");
 	}
 
 }
