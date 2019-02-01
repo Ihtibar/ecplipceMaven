f69e70afa658c81e7716c80a9c62032115cae696
Ihtibar <46764819+Ihtibar@users.noreply.github.com>
2019-02-01 14:27:51 -0500

    Create Map2.java

---
+++ Testings/src/test/java/Ihtibarnew/Testings/Map2.java
@@ -0,0 +1,14 @@
+public class Map2 {
+	public static void main(String[] args) {
+		 Map<Long, String> product= new HashMap<>();
+		 product.put(125L, "book");
+		 product.put(127L, "table");
+		 product.put(126L, "pen");
+		 product.put(122L, "bed");
+		 product.put(12322L, "pan");
+		 System.out.println(product);
+		 
+		 Set<Long> allkeys = product.keySet();
+		 System.out.println(allkeys);
+	}
+  }
